f2601ed80d6ace25b9814537ae94a605fcca6923
kyle misener <km.pacific.27@gmail.com>
2015-11-14 11:00:32 -0800

    add isPositiveInteger chks for correct userInput

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var isPositiveInteger =  function(userNumber) {
+  if (isNaN(userNumber) || (userNumber < 0)) {
+    return false;
+  } // end if
+  else {
+    return true;
+  } // end else
+}; // end isValidNumber
--- assets/spec/specs.js
+++ assets/spec/specs.js
@@ -1,1 +1,14 @@
 
+describe('isPositiveInteger', function() {
+  it('is false for any non-number input', function() {
+     expect(isPositiveInteger('a')).to.equal(false);
+  });
+
+  it('is true for any positive integer', function() {
+    expect(isPositiveInteger(9)).to.equal(true);
+  });
+
+  it('is false for any negative integer', function() {
+    expect(isPositiveInteger(-5)).to.equal(false);
+  });
+});
