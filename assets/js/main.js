d7655722c7b3e97b46af9ebd02b4515c9f980f6c
kyle misener <km.pacific.27@gmail.com>
2015-11-14 13:36:00 -0800

    add document.ready to output pingpong results to index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,59 @@ var isPositiveInteger =  function(userNumber) {
   else {
     return true;
   } // end else
-}; // end isValidNumber
+}; // end isPositiveInteger
+
+var pingPong = function(userNumber) {
+  if(userNumber%3 === 0 && userNumber%5 === 0) {
+    return 'pingpong';
+  } // end if
+  else if(userNumber%3 === 0) {
+    return 'ping';
+  } // end else if
+  else if(userNumber%5 === 0) {
+    return 'pong';
+  } // end else if
+  else {
+    return '';
+  } // end else
+}; // end pingPong
+
+$(document).ready(function() {
+  $("form#pingpong-number").submit(function(event) {
+    var userInput = parseInt($("input#user-input").val());
+    var isValidInput = isPositiveInteger(userInput);
+
+    $('#results').empty();
+
+    if(isValidInput) {
+      for(var i = 1; i <= userInput; i++) {
+
+        var result = pingPong(i);
+
+        switch(result) {
+          case "pingpong":
+            $("<li>pingpong</li>").appendTo("#results");
+            break;
+
+          case "ping":
+            $("<li>ping</li>").appendTo("#results");
+            break;
+
+          case "pong":
+            $("<li>pong</li>").appendTo("#results");
+            break;
+
+          default:
+            $("<li>"+i+"</li>").appendTo("#results");
+        } // end switch
+      } // end for
+    } // end if
+    else {
+      alert('Please enter a positive integer');
+      $('#results').empty();
+      $('#user-input').val('');
+    } // end else
+
+    event.preventDefault();
+  }); //end form submit
+}); // end document.ready()
--- assets/spec/specs.js
+++ assets/spec/specs.js
@@ -12,3 +12,61 @@ describe('isPositiveInteger', function() {
     expect(isPositiveInteger(-5)).to.equal(false);
   });
 });
+
+describe('pingPong', function() {
+  it('determines if userNumber is a multiple of both 3 and 5', function() {
+    expect(pingPong(15)).to.equal('pingpong');
+  });
+
+  it('determines if userNumber is a multiple of 3', function() {
+    expect(pingPong(9)).to.equal('ping');
+  });
+
+  it('determines if userNumber is a multiple of 5', function() {
+    expect(pingPong(25)).to.equal('pong');
+  });
+
+  it('determines if userNumber is not a multiple of 3 or 5', function() {
+    expect(pingPong(7)).to.equal('');
+  });
+});
+
+$(document).ready(function() {
+  $("form#pingpong-number").submit(function(event) {
+    var userInput = parseInt($("input#user-input").val());
+    var isValidInput = isPositiveInteger(userInput);
+
+    $('#results').empty();
+
+    if(isValidInput) {
+      for(var i = 1; i <= userInput; i++) {
+
+        var result = pingPong(i);
+
+        switch(result) {
+          case "pingpong":
+            $("<li>pingpong</li>").appendTo("#results");
+            break;
+
+          case "ping":
+            $("<li>ping</li>").appendTo("#results");
+            break;
+
+          case "pong":
+            $("<li>pong</li>").appendTo("#results");
+            break;
+
+          default:
+            $("<li>"+i+"</li>").appendTo("#results");
+        } // end switch
+      } // end for
+    } // end if
+    else {
+      alert('Please enter a positive integer');
+      $('#results').empty();
+      $('#user-input').val('');
+    } // end else
+
+    event.preventDefault();
+  }); //end form submit
+}); // end document.ready()
